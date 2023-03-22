9a1b4c82f17b0489fcebc14a8b5a0d91bdb3c7ba
Sonu Gupta <rg3029926@gmail.com>
2023-03-22 20:10:28 +0530

    youtub

--- src/Components/VideoDetails.jsx
+++ src/Components/VideoDetails.jsx
@@ -7,8 +7,7 @@ import { abbreviateNumber } from "js-abbreviation-number";
 
 import { fetchYoutubapi } from "../uliti/Api";
 import { Context } from "../context/Contextapi";
-// import SuggestionVideoCard from "./SuggestionVideoCard";
-
+import SuggestCard from "./SuggestCard";
 const VideoDetails = () => {
   const [video, setVideo] = useState();
   const [relatedVideo, setRelatedVideo] = useState();
@@ -18,13 +17,12 @@ const VideoDetails = () => {
   const fetchVideoDetels = () => {
 
     setLoading(true);
-    fetchYoutubapi(`video/details/${id}`).then((res) => {
+    fetchYoutubapi(`video/details/?id=${id}`).then((res) => {
       console.log(res);
       setVideo(res)
       setLoading(false)
     })
   }
-
   const fetchRelaVideos = () => {
     setLoading(true);
     fetchYoutubapi(`video/related-contents/?id=${id}`).then((res) => {
@@ -33,16 +31,17 @@ const VideoDetails = () => {
     })
   }
   useEffect(() => {
+    document.getElementById("root").classList.add("custom-h");
     fetchVideoDetels();
     fetchRelaVideos();
   }, [])
   return (
     <div className="flex justify-center flex-row h-[calc(100%-56px)] bg-black">
       <div className="w-full max-w-[1280px] flex flex-col lg:flex-row ">
-        <div className="flex flext-col lg:w-[calc(100%-350px)] xl:w-[calc100%-400px] px-4 py-3 lg:py-6 overflow-y-auto" >
+        <div className="flex flex-col lg:w-[calc(100%-350px)] xl:w-[calc(100%-400px)] px-4 py-3 lg:py-6 overflow-y-auto" >
           <div className="h-[200px] md:h-[400px] lg:h-[550px] ml-[-16px] lg:ml-0 mr-[-16px] lg:mr-0 ">
             <ReactPlayer
-              url={`https://www.youtube.com/watch?v${id}`}
+              url={`https://www.youtube.com/watch?v=${id}`}
               controls
               width="100%"
               height="100%"
@@ -52,10 +51,10 @@ const VideoDetails = () => {
           <div className=" text-white font-bold text-sm md:text-xl mt-4 line-clamp-2 ">
             {video?.title}
           </div>
-          <div className="flext justify-between flex-col md:flex-row mt-4 ">
+          <div className="flex justify-between flex-col md:flex-row mt-4">
             <div className="flex">
               <div className="flex items-start">
-                <div className="flex h-11 rounded-full overflow-hidden">
+                <div className="flex h-11 w-11 rounded-full overflow-hidden">
                   <img className="h-full w-full object-cover" src={video?.author?.avatar[0]?.url} alt="" />
                 </div>
               </div>
@@ -85,6 +84,19 @@ const VideoDetails = () => {
             </div>
           </div>
         </div>
+
+        <div className="flex flex-col py-6 px-4 overflow-y-auto lg:w-[350px] xl:w-[400px]">
+                    {relatedVideo?.contents?.map((item, index) => {
+                        if (item?.type !== "video") return false;
+                        return (
+                            <SuggestCard
+                                key={index}
+                                video={item?.video}
+                            />
+                        );
+                    })}
+                </div>
+
       </div>
 
     </div>
